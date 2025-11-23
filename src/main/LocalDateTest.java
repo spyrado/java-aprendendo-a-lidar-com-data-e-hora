e3eee237fe9aa4b4b4616365dad911032eb79d4f
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2025-11-23 09:48:05 -0300

    feat: utilizando LocalDate e LocalDateTime

--- src/main/LocalDateTest.java
+++ src/main/LocalDateTest.java
@@ -2,13 +2,16 @@ package main;
 
 import java.time.LocalDate;
 
-public class Main {
+public class LocalDateTest {
   public static void main(String[] args) {
     LocalDate dataCompra = LocalDate.of(2025, 11, 23);
     LocalDate dataPrimeiraParcela = dataCompra.plusDays(15);
     LocalDate dataSegundaParcela = dataPrimeiraParcela.plusDays(30);
 
     var dataAtual = LocalDate.now();
+
+    System.out.println("=============== [INICIO] VERIFICANDO O ESTADO QUE SE ENCONTRA A PARCELA ===============");
+
     if(dataPrimeiraParcela.isEqual(dataAtual)) {
       System.out.println("Dia do vencimento da primeira parcela.");
     } else if(dataPrimeiraParcela.isBefore(dataAtual)) {
@@ -17,8 +20,15 @@ public class Main {
       System.out.println("Primeira parcela ainda não venceu.");
     }
 
+    System.out.println("=============== [FIM] VERIFICANDO O ESTADO QUE SE ENCONTRA A PARCELA ===============");
+
+    System.out.println("=============== [INICIO] LOGANDO AS DATAS ===============");
+
+    System.out.println("Data atual: " + dataAtual);
     System.out.println("Data compra: " + dataCompra);
     System.out.println("Data primeira parcela: " + dataPrimeiraParcela);
     System.out.println("Data segunda parcela: " + dataSegundaParcela);
+
+    System.out.println("=============== [FIM] LOGANDO AS DATAS ===============");
   }
 }
---
+++ src/main/LocalDateTimeTest.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.time.LocalDateTime;
+
+public class LocalDateTimeTest {
+
+  public static void main(String[] args) {
+    LocalDateTime localDateTime = LocalDateTime.now();
+
+    System.out.println(localDateTime);
+  }
+}
